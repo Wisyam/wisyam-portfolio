83770e03f8397d328c3e608aef72c981beff00b4
icam <wisyamamanullah@gmail.com>
2026-08-16 01:55:57 +0700

    feat: scaffold Vite + React + TS + Tailwind + R3F with CI

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { SceneCanvas } from './components/SceneCanvas'
+
+function App() {
+  return (
+    <main className="h-dvh w-screen overflow-hidden bg-slate-900">
+      <SceneCanvas />
+    </main>
+  )
+}
+
+export default App
